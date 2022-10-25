82eb99789c83327d405893be21cfaa2d885facf8
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2022-10-25 17:12:29 +0700

    feat(utils): implement download and install utilities

--- lib/utils.bash
+++ lib/utils.bash
@@ -7,86 +7,211 @@ set -euo pipefail
 GH_REPO="https://github.com/apache/spark"
 SPARK_ARCHIVE_URL='https://archive.apache.org/dist/spark'
 TOOL_NAME='spark'
-TOOL_TEST='spark --help'
+TOOL_TEST='spark-shell --help'
 
 DEFAULT_CURL_OPTS=(-fsSL)
+DEFAULT_SPARK_VERSION='3.3.0'
 
-##############################################
+##################################################
 # Print error message with generalized format.
-# Globals:
+#
 # Arguments:
-#   - Error message
+#   $* - Error message
 # Outputs:
 #   A defined error message text with exit code 1.
-##############################################
+##################################################
 fail() {
-  echo -e "asdf-$TOOL_NAME: $*"
+  echo -e "asdf-${TOOL_NAME}: $*"
   exit 1
 }
 
-###########################################
+#################################################
 # List all available Apache Spark versions.
-# Globals:
-#   None
+#
 # Arguments:
-#   - Apache spark archive HTML content or the HTML file path.
+#   $1 - Apache spark archive HTML content.
 # Outputs:
-#   A space-delimited version string.
-###########################################
+#   All available version with newline delimited.
+#################################################
 list_all_versions() {
-  local archive_arg="${1:-}"
-  local archive_src=''
+  local archive_src="${1:-}"
 
-  if [[ -z "${archive_arg}" ]]; then
+  if [[ -z "${archive_src}" ]]; then
     echo ""
     return
   fi
 
-  if [[ -f "${archive_arg}" ]]; then
-    archive_src="$(cat "${archive_arg}")"
-  else
-    archive_src="${archive_arg}"
+  { grep -Eo '<a href="spark-[0-9]+\.[0-9]+\.[0-9]+(\-[a-z0-9]+)?\/">' <<<"${archive_src}" || :; } |
+    sed -E 's/^<a href="spark-(.+)\/">$/\1/g'
+}
+
+######################################################
+# Scan available spark pre-built binaries archives.
+#
+# Arguments:
+#   $1 - Apache Spark version.
+#   $2 - Spark archive HTML page content.
+# Outputs:
+#   All available spark archives separated by newline.
+######################################################
+scan_available_archives() {
+  local spark_version="${1:-"${DEFAULT_SPARK_VERSION}"}"
+  local html_content="${2:-}"
+
+  if [[ -z "${html_content}" ]]; then
+    echo ""
+    return
   fi
 
-  echo "${archive_src}" |
-    { grep -Eo '<a href="spark-[0-9]+\.[0-9]+\.[0-9]+(\-[a-z0-9]+)?\/">' || :; } |
-    sed -E 's/^<a href="spark-(.+)\/">$/\1/g' |
-    xargs
+  { grep -Eo "<a href=\"spark-${spark_version}-bin-(.+)\.tgz\">" <<<"${html_content}" || :; } |
+    sed -E 's/^<a href="(.+)">$/\1/g'
 }
 
-download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
+##############################################################
+# Construct Apache Spark prebuilt binary archive download URL.
+#
+# Arguments:
+#   $1 - Apache Spark version.
+#   $2 - Archive filename.
+# Outputs:
+#   A valid apache spark prebuilt binary archive download URL.
+##############################################################
+construct_release_archive_url() {
+  local spark_version="${1:-}"
+  local archive_filename="${2:-}"
 
-  # TODO: Adapt the release URL convention for spark
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  if [[ -z "${spark_version}" ]]; then
+    fail "Apache Spark version is required."
+  fi
 
-  echo "* Downloading $TOOL_NAME release $version..."
-  curl "${DEFAULT_CURL_OPTS[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  if [[ -z "${archive_filename}" ]]; then
+    fail "Spark binary archive filename is required."
+  fi
+
+  echo "${SPARK_ARCHIVE_URL}/spark-${spark_version}/${archive_filename}"
 }
 
+##################################################################
+# Get Apache Spark prebuilt binary archive filename based on
+# the specified environment variable (if any).
+#
+# Globals:
+#   ASDF_SPARK_HADOOP_VERSION - Custom hadoop version for current
+#                               Apache Spark version.
+#   ASDF_SPARK_WITHOUT_HADOOP - Prefer archive without hadoop support?
+# Arguments:
+#   $1 - Apache Spark version.
+#   $2 - Archive filename.
+# Outputs:
+#   A valid apache spark prebuilt binary archive download URL.
+##################################################################
+get_release_archive_filename() {
+  local install_type="${1:-'version'}"
+  local spark_version="${2:-"${DEFAULT_SPARK_VERSION}"}"
+  local archive_download_content="${3:-}"
+  local custom_hadoop_version="${ASDF_SPARK_HADOOP_VERSION:-}"
+  local without_hadoop="${ASDF_SPARK_WITHOUT_HADOOP:-0}"
+
+  local available_archives available_archives_array without_hadoop_archive_filename \
+    with_hadoop_archive_filename available_archives_array_length
+
+  if [[ "${install_type}" != "version" ]]; then
+    fail "asdf-$TOOL_NAME only supports version release installation"
+  fi
+
+  available_archives="$(scan_available_archives "${spark_version}" "${archive_download_content}")"
+
+  # Get without-hadoop archive filename
+  if [[ ! "${without_hadoop}" =~ [0|f|false] ]]; then
+    without_hadoop_archive_filename="$({ grep "spark-${spark_version}-bin-without-hadoop\.tgz" <<<"${available_archives}" || :; } | xargs)"
+    if [[ -n "${without_hadoop_archive_filename}" ]]; then
+      echo "${without_hadoop_archive_filename}"
+      return
+    else
+      fail "Apache Spark ${spark_version} does not have without-hadoop archive."
+    fi
+  fi
+
+  # Get custom hadoop version when users provide it.
+  if [[ -n "${custom_hadoop_version}" ]]; then
+    with_hadoop_archive_filename="$(
+      { grep -o "spark-${spark_version}-bin-hadoop${custom_hadoop_version}\.tgz" <<<"${available_archives}" || :; } | xargs
+    )"
+    if [[ -n "${with_hadoop_archive_filename}" ]]; then
+      echo "${with_hadoop_archive_filename}"
+      return
+    else
+      fail "Unfortunately, Apache Spark with Hadoop ${custom_hadoop_version} is not available yet."
+    fi
+  fi
+
+  # Get the latest hadoop version when users do not provide it.
+  with_hadoop_archive_filename="$(
+    { grep -Eo "spark-${spark_version}-bin-hadoop[0-9](\.[0-9])?\.tgz" <<<"${available_archives}" || :; } | xargs
+  )"
+  read -r -a available_archives_array <<<"${with_hadoop_archive_filename}"
+
+  available_archives_array_length="${#available_archives_array[@]}"
+  if [[ "${available_archives_array_length}" -gt "0" ]]; then
+    echo "${available_archives_array[$((available_archives_array_length - 1))]}"
+  else
+    fail "Unfortunately, current Apache Spark version does not provide hadoop support prebuilt binary archive."
+  fi
+}
+
+##################################################################
+# Download Apache Spark prebuilt binary archive into provided
+# ASDF_DOWNLOAD_PATH.
+#
+# Arguments:
+#   $1 - Apache spark archive download URL.
+#   $2 - Apache spark archive target path.
+# Outputs:
+#   Exit code 0 if successfully downloading the archive file, else
+#   1 with error message.
+##################################################################
+download_archive() {
+  local download_url="${1:-}"
+  local target_filepath="${2:-}"
+
+  echo "* Downloading ${target_filepath##*/}..."
+  curl "${DEFAULT_CURL_OPTS[@]}" -o "${target_filepath}" -C - "${download_url}" || fail "Could not download ${download_url}"
+}
+
+##################################################################
+# Extract and install Apache Spark prebuilt binary archive into
+#  provided asdf install path.
+#
+# Arguments:
+#   $1 - asdf install type.
+#   $2 - Apache Spark version.
+#   $3 - asdf install path.
+# Outputs:
+#   Exit code 0 if successfully installing the archive file, else
+#   1 with error message.
+##################################################################
 install_version() {
-  local install_type="$1"
-  local version="$2"
-  local install_path="${3%/bin}/bin"
+  local install_type="${1:-}"
+  local version="${2:-}"
+  local install_path="${3:-}"
 
-  if [ "$install_type" != "version" ]; then
-    fail "asdf-$TOOL_NAME supports release installs only"
+  local tool_cmd
+
+  if [[ "${install_type}" != "version" ]]; then
+    fail "asdf-$TOOL_NAME only supports version release installation"
   fi
 
   (
-    mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    mkdir -p "${install_path}"
+    cp -r "${ASDF_DOWNLOAD_PATH}/*" "${install_path}"
 
     # TODO: Assert spark executable exists.
-    local tool_cmd
-    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+    tool_cmd="$(echo "${TOOL_TEST}" | cut -d' ' -f1)"
+    [[ -x "${install_path}/bin/${tool_cmd}" ]] || fail "Expected ${install_path}/bin/${tool_cmd} to be executable."
 
-    echo "$TOOL_NAME $version installation was successful!"
+    echo "${TOOL_NAME}-${version} installation was successful!"
   ) || (
-    rm -rf "$install_path"
-    fail "An error occurred while installing $TOOL_NAME $version."
+    rm -rf "${install_path}"
+    fail "An error occurred while installing ${TOOL_NAME}-${version}."
   )
 }
--- test/util.bats
+++ test/util.bats
@@ -4,32 +4,151 @@
 
 set -euo pipefail
 
-CURRENT_DIR="$(cd "$(dirname "$BATS_TEST_FILENAME")" &>/dev/null && pwd)"
+UTIL_TEST_DIR="$(cd "$(dirname "$BATS_TEST_FILENAME")" &>/dev/null && pwd)"
 
-load "${CURRENT_DIR}/../lib/utils.bash"
-load "${CURRENT_DIR}/resources/spark_archive_mock.bats"
+load "${UTIL_TEST_DIR}/../lib/utils.bash"
+load "${UTIL_TEST_DIR}/resources/spark_archive_mock.bash"
+
+# Override DEFAULT_SPARK_VERSION variable for testing purpose
+DEFAULT_SPARK_VERSION='3.3.0'
+
+#region fail
+@test "fail: it should show an error message with exit status code 1" {
+  local error_message='Lorem ipsum dolor sip amet'
+  run fail "${error_message}"
+  [ "${status}" -eq 1 ]
+  [ "${output}" = "asdf-spark: ${error_message}" ]
+}
+
+@test "fail: it should show an error message from multiple arguments with exit status code 1" {
+  local error_message='Lorem ipsum dolor sip amet'
+  local error_message_2='My time has come'
+  run fail "${error_message}" "${error_message_2}"
+  [ "${status}" -eq 1 ]
+  [ "${output}" = "asdf-spark: ${error_message} ${error_message_2}" ]
+}
+#endregion fail
 
 #region list_all_versions
-@test "list_all_versions: it should return the correct list-all format from HTML file" {
-  result="$(list_all_versions "${CURRENT_DIR}/resources/spark_archive.html")"
-  expected_result="0.8.0-incubating 0.9.0-incubating 0.9.2 1.0.0 1.6.3 \
-2.0.0-preview 2.0.0 2.4.8 3.0.0-preview 3.0.0-preview2 3.0.0 3.3.0"
-  [ "${result}" = "${expected_result}" ]
-}
-
 @test "list_all_versions: it should return the correct list-all format from HTML content" {
-  result="$(list_all_versions "${spark_archive_html_content_mock:=}")"
-  expected_result="3.0.0-preview 3.0.0-preview2 3.0.0 3.3.0"
-  [ "${result}" = "${expected_result}" ]
+  run list_all_versions "${spark_archive_html_content_mock:-}"
+  expected_result="0.8.0-incubating
+0.9.0-incubating
+0.9.2
+1.0.0
+1.6.3
+2.0.0-preview
+2.0.0
+2.4.8
+3.0.0-preview
+3.0.0-preview2
+3.0.0
+3.3.0"
+  [ "${status}" -eq 0 ]
+  [ "${output}" = "${expected_result}" ]
 }
 
-@test "list_all_versions: it should return empty string when no archive page is provided" {
-  result="$(list_all_versions)"
-  [ "${result}" = "" ]
+@test "list_all_versions: it should return empty string when no archive HTML content is provided" {
+  run list_all_versions
+  [ "${status}" -eq 0 ]
+  [ -z "${output}" ]
 }
 
-@test "list_all_versions: it should return empty string when invalid file path is provided" {
-  result="$(list_all_versions 'asdf')"
-  [ "${result}" = "" ]
+@test "list_all_versions: it should return empty string when no version is matched from the HTML content" {
+  run list_all_versions 'Lorem ipsum sip dolor amet'
+  [ "${status}" -eq 0 ]
+  [ -z "${output}" ]
 }
 #endregion list_all_versions
+
+#region scan_available_archives
+@test "scan_available_archives: it should return all available spark binary archives from the dist HTML content" {
+  run scan_available_archives "3.3.0" "${spark_330_download_html_content_mock:-}"
+  expected_result="spark-3.3.0-bin-hadoop2.tgz
+spark-3.3.0-bin-hadoop3-scala2.13.tgz
+spark-3.3.0-bin-hadoop3.tgz
+spark-3.3.0-bin-without-hadoop.tgz"
+  [ "${status}" -eq 0 ]
+  [ "${output}" = "${expected_result}" ]
+}
+
+@test "scan_available_archives: it should use DEFAULT_SPARK_VERSION when no spark version is specified" {
+  run scan_available_archives "" "${spark_330_download_html_content_mock:-}"
+  [ "${status}" -eq 0 ]
+  [ -n "${output}" ]
+}
+
+@test "scan_available_archives: it should return empty string when no available spark binary archives is available" {
+  run scan_available_archives "3.4.0" "${spark_330_download_html_content_mock:-}"
+  [ "${status}" -eq 0 ]
+  [ -z "${output}" ]
+}
+#endregion scan_available_archives
+
+#region construct_release_archive_url
+@test "construct_release_archive_url: it should return a valid prebuilt binary download URL" {
+  local archive_filename='example.tgz'
+  run construct_release_archive_url "${DEFAULT_SPARK_VERSION}" "${archive_filename}"
+  [ "${status}" -eq 0 ]
+  [ "${output}" = "${SPARK_ARCHIVE_URL}/spark-${DEFAULT_SPARK_VERSION}/${archive_filename}" ]
+}
+
+@test "construct_release_archive_url: it should show an error message when no spark version is specified" {
+  local archive_filename='example.tgz'
+  run construct_release_archive_url "" "${archive_filename}"
+  [ "${status}" -eq 1 ]
+  [ "${output}" = "asdf-spark: Apache Spark version is required." ]
+}
+
+@test "construct_release_archive_url: it should show an error message when no archive filename is specified" {
+  run construct_release_archive_url "${DEFAULT_SPARK_VERSION}" ""
+  [ "${status}" -eq 1 ]
+  [ "${output}" = "asdf-spark: Spark binary archive filename is required." ]
+}
+#endregion construct_release_archive_url
+
+#region get_release_archive_filename
+@test "get_release_archive_filename: it should return the latest hadoop version URL (ASDF_SPARK_HADOOP_VERSION is not set)" {
+  run get_release_archive_filename "version" "${DEFAULT_SPARK_VERSION}" "${spark_330_download_html_content_mock:-}"
+  echo "${output}"
+  [ "${status}" -eq 0 ]
+  [ "${output}" = "spark-${DEFAULT_SPARK_VERSION}-bin-hadoop3.tgz" ]
+}
+
+@test "get_release_archive_filename: it should return the latest hadoop version URL (ASDF_SPARK_HADOOP_VERSION is set)" {
+  ASDF_SPARK_HADOOP_VERSION=2 run get_release_archive_filename "version" "${DEFAULT_SPARK_VERSION}" "${spark_330_download_html_content_mock:-}"
+  [ "${status}" -eq 0 ]
+  [ "${output}" = "spark-${DEFAULT_SPARK_VERSION}-bin-hadoop2.tgz" ]
+}
+
+@test "get_release_archive_filename: it should return error message when the install type is not version based" {
+  run get_release_archive_filename "ref" "${DEFAULT_SPARK_VERSION}" "${spark_330_download_html_content_mock:-}"
+  [ "${status}" -eq 1 ]
+  [ "${output}" = "asdf-spark: asdf-spark only supports version release installation" ]
+}
+
+@test "get_release_archive_filename: it should return error message when no any prebuilt binary archive available" {
+  run get_release_archive_filename "version" "${DEFAULT_SPARK_VERSION}" "${spark_111_broken_download_html_content_mock:-}"
+  [ "${status}" -eq 1 ]
+  [ "${output}" = "asdf-spark: Unfortunately, current Apache Spark version does not provide hadoop support prebuilt binary archive." ]
+}
+
+@test "get_release_archive_filename: it should return error message when the custom hadoop version is not available" {
+  ASDF_SPARK_HADOOP_VERSION='2.3' run get_release_archive_filename "version" "${DEFAULT_SPARK_VERSION}" "${spark_330_download_html_content_mock:-}"
+  [ "${status}" -eq 1 ]
+  [ "${output}" = "asdf-spark: Unfortunately, Apache Spark with Hadoop 2.3 is not available yet." ]
+}
+
+@test "get_release_archive_filename: it should return without hadoop version URL when ASDF_SPARK_WITHOUT_HADOOP is set" {
+  ASDF_SPARK_WITHOUT_HADOOP=1 run get_release_archive_filename "version" "${DEFAULT_SPARK_VERSION}" "${spark_330_download_html_content_mock:-}"
+  [ "${status}" -eq 0 ]
+  [ "${output}" = "spark-${DEFAULT_SPARK_VERSION}-bin-without-hadoop.tgz" ]
+}
+
+@test "get_release_archive_filename: it should return error message when there is no without-hadoop version available" {
+  local spark_version='1.1.1'
+  ASDF_SPARK_WITHOUT_HADOOP=1 run get_release_archive_filename "version" "1.1.1" "${spark_111_download_html_content_mock:-}"
+  [ "${status}" -eq 1 ]
+  [ "${output}" = "asdf-spark: Apache Spark ${spark_version} does not have without-hadoop archive." ]
+}
+#endregion get_release_archive_filename
